3ab0b26ac030db5b6ba35b5d77d2c87738106b83
Siarhei <greysiarhei@gmail.com>
2023-05-07 11:37:57 +0300

    Я создал новый репозиторий

---
+++ src/package1/Ex2.java
@@ -0,0 +1,14 @@
+package package1;
+
+public class Ex2 {
+    //psvm + Enter = public static void main(String[] args) {}
+
+//    sout + Enter = System.out.println();
+
+    // комментарий строчный Cntrl + /
+    // комментарий блочный Cntrl + Shift + /
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
---
+++ src/package1/StartJava.java
@@ -0,0 +1,9 @@
+package package1;
+
+public class StartJava {
+    //CTRL+ALT+L windows реформат кода
+    //Command+Options+L Mac реформат кода
+    public static void main(String[] args) {
+        System.out.println("Hello all QA-4");
+    }
+}
