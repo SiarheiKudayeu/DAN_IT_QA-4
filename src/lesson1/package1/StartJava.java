68704b5fdd0b8c78768b811ff25200abfe2b67e0
Siarhei <greysiarhei@gmail.com>
2023-05-09 18:48:18 +0300

    добавил часть урока2

--- src/lesson1/package1/StartJava.java
+++ src/lesson1/package1/StartJava.java
@@ -1,9 +1,14 @@
 package lesson1.package1;
 
+import java.util.Random;
+
 public class StartJava {
     //CTRL+ALT+L windows реформат кода
     //Command+Options+L Mac реформат кода
     public static void main(String[] args) {
         System.out.println("Hello all QA-4");
+        Random random = new Random();
+        System.out.println(random.nextInt(100));
+        System.out.println((int) (Math.random() * 100)-50);
     }
 }
---
+++ src/lesson2/Primitives.java
@@ -0,0 +1,12 @@
+package lesson2;
+
+public class Primitives {
+    public static void main(String[] args) {
+        //byte 128 - 127
+        //short 32768 - 32767
+        //int -2^31 до 2^31-1
+        //long -2^63 до 2^63-1
+
+
+    }
+}
